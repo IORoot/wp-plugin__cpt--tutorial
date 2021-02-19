0341b97e9f401eff7e2e3dc7f7da7226b1ede6eb
Andy Pearson <andy@londonparkour.com>
2021-02-19 11:25:31 +0000

    taxonomy fix

--- src/filters/register_template_folder.php
+++ src/filters/register_template_folder.php
@@ -22,7 +22,8 @@ class register_template_folder
 
     public function taxonomy_template($template) {
 
-        global $post_type;
+        global $post;
+        $post_type = $post->post_type;
 
         if ($post_type != $this->post_type){ return $template; }
 
--- src/views/template-parts/taxonomy_hero.php
+++ src/views/template-parts/taxonomy_hero.php
@@ -4,7 +4,7 @@
         <div class="h-full bg-cover bg-no-repeat shadow-2xl rounded-xl md:w-1/2 lg:w-1/3 xl:w-1/4" style="background-image:url('<?php echo $image_url['url']; ?>');"> </div>
 
         <div class="flex-1 flex-col self-left md:self-center mt-2 md:m-4 xl:m-8">
-            <div class="text-xl md:text-4xl"><?php echo $term->name; ?></div>
+            <div class="text-xl md:text-4xl"><?php echo ucfirst($term->name); ?></div>
             <div class="py-2 hidden lg:block"><?php echo $term->description; ?></div>
             <div class="text-sm font-smoke"><?php echo count($posts); ?> videos in the <?php echo strtolower($term->name); ?> series.</div>
         </div>
