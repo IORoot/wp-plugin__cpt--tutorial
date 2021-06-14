8e65877a9ac95df4340217e259fa11ffd8a7ab06
Andy Pearson <andy@londonparkour.com>
2021-06-14 15:55:55 +0100

    cat hero

--- src/views/tag-parts/category_hero.php
+++ src/views/tag-parts/category_hero.php
@@ -22,12 +22,12 @@
     // │                                                                         │
     // └─────────────────────────────────────────────────────────────────────────┘
     ?>
-    <svg class="z-50 absolute fill-gray-500 top-10 left-10 w-10 h-10">
+    <svg class="z-50 absolute fill-gray-500 top-4 md:top-10 left-4 md:left-10 w-10 h-10">
 		<use xlink:href="#<?php echo $svg_name; ?>"></use>
 	</svg>
 
 
-    <div class="z-40 absolute bottom-10 left-10 w-1/2">
+    <div class="z-40 absolute bottom-0 left-0 p-4 md:p-10 w-full md:w-1/2">
 
         <?php
         // ┌─────────────────────────────────────────────────────────────────────────┐
@@ -36,7 +36,7 @@
         // │                                                                         │
         // └─────────────────────────────────────────────────────────────────────────┘
         ?>
-        <h1 class="font-semibold text-7xl font-serif"><?php echo ucfirst($current_term->name); ?></h1>
+        <h1 class="font-semibold text-4xl md:text-7xl font-serif"><?php echo ucfirst($current_term->name); ?></h1>
 
         <?php
         // ┌─────────────────────────────────────────────────────────────────────────┐
@@ -65,7 +65,7 @@
         // │                                                                         │
         // └─────────────────────────────────────────────────────────────────────────┘
         ?>
-        <p class="font-light w-3/4 mt-4"><?php echo $current_term->acf['meta_fields']['Excerpt']; ?></p>
+        <p class="font-light w-full md:w-3/4 mt-4"><?php echo $current_term->acf['meta_fields']['Excerpt']; ?></p>
     </div>
 
     <?php
@@ -75,7 +75,7 @@
     // │                                                                         │
     // └─────────────────────────────────────────────────────────────────────────┘
     ?>
-    <img class="z-30 absolute top-0 right-40 h-96" src="<?php echo $current_term_acf_image; ?>">
+    <img class="z-30 absolute top-0 right-0 lg:right-40 h-72 sm:h-80 md:h-96" src="<?php echo $current_term_acf_image; ?>">
 
     <?php
     // ┌─────────────────────────────────────────────────────────────────────────┐
@@ -84,7 +84,7 @@
     // │                                                                         │
     // └─────────────────────────────────────────────────────────────────────────┘
     ?>
-    <div class="z-20 wavey-min bg-right-bottom bg-no-repeat absolute right-0 bottom-0 mix-blend-screen" style="width:150%; height:150%"></div>
+    <div class="z-20 wavey-min bg-right-bottom bg-no-repeat absolute right-0 bottom-0 mix-blend-screen hidden md:block" style="width:150%; height:150%"></div>
 
     <?php
     // ┌─────────────────────────────────────────────────────────────────────────┐
@@ -93,7 +93,7 @@
     // │                                                                         │
     // └─────────────────────────────────────────────────────────────────────────┘
     ?>
-    <svg class="z-10 absolute fill-green-600 -top-1/2 -left-1/2 " style="width:200%; height:200%;">
+    <svg class="z-10 absolute fill-green-600 -top-1/2 -left-1/2 opacity-50 md:opacity-100" style="width:200%; height:200%;">
 		<use xlink:href="#<?php echo $svg_name; ?>"></use>
 	</svg>
 
